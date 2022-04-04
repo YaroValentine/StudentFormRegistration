6aaadc7cbe34ad89ff06988c485cb17f98a0db76
Yaroslav Vinokurov <yaroslav.vinokurov@gmail.com>
2022-04-04 12:40:49 -0400

    Updated TestBase added @AfterEach attachments

--- src/test/java/qa/TestBase.java
+++ src/test/java/qa/TestBase.java
@@ -4,28 +4,36 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import qa.app.AppManager;
+import qa.helpers.Attach;
 
 import static qa.app.AppManager.errorList;
 
 public class TestBase {
-    private static AppManager app;
-
 
     @BeforeAll
     static void setUp() {
         SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.browserSize = "1980x1200";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        app = new AppManager();
-        app.init();
+
+        new AppManager().init();
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last Screenshot");
+        Attach.addVideo();
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
     }
 
     @AfterAll

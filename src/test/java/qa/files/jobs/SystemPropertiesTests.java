bfc5856d13262a3bb9f54fc0324780298b8ba763
Yaroslav Vinokurov <yaroslav.vinokurov@gmail.com>
2022-04-04 17:16:13 -0400

    Added properties tests

---
+++ src/test/java/qa/files/jobs/SystemPropertiesTests.java
@@ -0,0 +1,29 @@
+package qa.files.jobs;
+
+import org.junit.jupiter.api.Test;
+
+public class SystemPropertiesTests {
+    @Test
+    void test1() {
+        String browser = System.getProperty("browser");
+        System.out.println(browser);
+    }
+
+    @Test
+    void test2() {
+        System.setProperty("browser2", "Safari");
+        String browser = System.getProperty("browser2");
+        System.out.println(browser);
+    }
+
+    @Test
+    void test3() {
+        String browser = System.getProperty("browser", "Opera");
+        System.out.println(browser);
+    }
+
+    @Test
+    void test4() {
+        System.out.println("I'd like to say " + System.getProperty("say"));
+    }
+}
